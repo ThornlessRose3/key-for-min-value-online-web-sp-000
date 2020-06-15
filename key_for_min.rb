a518bbbd3bf1aa20bfdf1aa76fffcdaac7396070
Carrie Jones <c.jessica.jones@gmail.com>
2020-06-15 20:33:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,5 +8,14 @@ def key_for_min_value(name_hash)
     key_names << key
     values << value
   end
-  return key_names
+  test_value = values[0]
+  min_i = 0
+  i = 0
+  while i < values.length
+    if value[i] <= test_value
+      min_i = i
+    end
+    i += 1
+  end
+  return key_names[min_i]
 end
